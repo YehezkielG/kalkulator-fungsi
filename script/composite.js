f9bdadaad17b37516e399f8d2404babc13bb5986
YehezkielG <112444049+YehezkielG@users.noreply.github.com>
2024-11-04 11:29:06 +0700

    ui/ux update

--- script/composite.js
+++ script/composite.js
@@ -1,49 +1,21 @@
-var fCompositeField = document.getElementById('fComposite');
-var gCompositeField = document.getElementById('gComposite');
-var resultField = document.getElementById("resultComposite");
-var btn = document.querySelectorAll("btn");
-
-var MQ = MathQuill.getInterface(2); 
-
-var fCompositeField = MQ.MathField(fCompositeField, {
-  spaceBehavesLikeTab: true, 
-  handlers: {
-    edit: function(){
-      btn.forEach((btn,index)=>{
-        btn.addEventListener("click",(value)=>{
-          fCompositeField.latex(fCompositeField.latex()+btn.textContent);
-        })
-      });
-    }
-  }
-});
-var gCompositeField = MQ.MathField(gCompositeField,{
-    spaceBehavesLikeTab:true,
-    Headers:{
-    edit:function(){ }
-    }
-})
-
-var resultField = MQ.StaticMath(resultField)
-
-function calculate(){
-  //get latex code
-  let f = fCompositeField.latex();
-  let g = gCompositeField.latex();
-  f = Tojs(f);g = Tojs(g)
-  //subtitution
-  f = f.replaceAll('x', "("+g+")");
-  //evaluate
-  console.log(f);
-  let evaluate = algebra.parse(f).toTex().toString();
-  resultField.latex(evaluate)
-}
-
-function Tojs(str){
+function Tojs(str) {
   return str.replaceAll(/\\frac{([^}]+)}{([^}]+)}/g, '($1)/($2)') // Ubah pecahan
-  .replaceAll(/\\sqrt{([^}]+)}/g, 'sqrt($1)')             // Ubah akar
+    .replaceAll(/\\sqrt{([^}]+)}/g, 'sqrt($1)')             // Ubah akar
   // .replaceAll(/\\left\(/g, '(')                           
   // .replaceAll(/\\right\)/g, ')')                          
   // .replaceAll(/\\cdot/g, '*')                             
   // .replaceAll(/\^/g, '**');  
+}
+
+function calculate() {
+  //get latex code
+  let f = fCompositeField.latex();
+  let g = gCompositeField.latex();
+  f = Tojs(f); g = Tojs(g)
+  //subtitution
+  f = f.replaceAll('x', "(" + g + ")");
+  //evaluate
+  console.log(f);
+  let evaluate = algebra.parse(f).toTex().toString();
+  resultField.latex(evaluate)
 }
--- script/ui-uxScript.js
+++ script/ui-uxScript.js
@@ -1,9 +1,13 @@
+var fCompositeField = document.getElementById('fComposite');
+var gCompositeField = document.getElementById('gComposite');
+var resultField = document.getElementById("resultComposite");
+var btn = document.querySelectorAll("#btn");
 const compositionBtn = document.getElementById("comFunc")
 const inverseBtn = document.getElementById("invFunc")
-
 const  composition = document.getElementById("composition");
 const  inverse = document.getElementById("inverse");
 
+//change from composite fucntion to inverse function 
 inverseBtn.addEventListener("click",function(){
     this.classList.remove("opacity-40");
     compositionBtn.classList.add("opacity-40");
@@ -11,7 +15,7 @@ inverseBtn.addEventListener("click",function(){
     inverse.classList.remove("hidden");
     composition.classList.add("hidden");
 })
-
+//change from inverse function to composite funtion
 compositionBtn.addEventListener("click",function(){
     this.classList.remove("opacity-40");
     inverseBtn.classList.add("opacity-40");
@@ -19,3 +23,36 @@ compositionBtn.addEventListener("click",function(){
     inverse.classList.add("hidden");
     composition.classList.remove("hidden");
 })
+
+
+var MQ = MathQuill.getInterface(2);
+
+var fCompositeField = MQ.MathField(fCompositeField, {
+  spaceBehavesLikeTab: true,
+  handlers: {
+    edit: function () {}
+  }
+});
+var gCompositeField = MQ.MathField(gCompositeField, {
+  spaceBehavesLikeTab: true,
+  Headers: {
+    edit: function () {}
+  }
+})
+
+var resultField = MQ.StaticMath(resultField)
+
+
+btn.forEach((btn, index) => {
+    btn.addEventListener("mousedown", (value) => {
+        event.preventDefault();
+        if(document.getElementById('fComposite').classList.contains('mq-focused')){
+            fCompositeField.write(btn.value);
+        }
+        else if(document.getElementById('gComposite').classList.contains('mq-focused')){
+            gCompositeField.write(btn.value);
+        }
+    })
+});
+
+MQ.StaticMath(document.querySelector('.xbtn'));
